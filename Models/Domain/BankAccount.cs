28817d6a6b4537700d6c27516fa89363f04215a0
Victor Koopman <victor.koopman@student.hogent.be>
2020-12-08 19:15:54 +0100

    BankAccount.cs aangemaakt

---
+++ Models/Domain/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Banking.Models.Domain
+{
+    public class BankAccount
+    {
+        #region Fields
+        private string _accountNumber;
+        private decimal _balance;
+        #endregion
+
+        #region Properties
+        public decimal Balance { get; private set; }
+        public string AccountNumber { get; }
+        #endregion
+
+        #region Constructors
+        public BankAccount(string account)
+        {
+            AccountNumber = account;
+            Balance = Decimal.Zero;
+        }
+        #endregion
+
+        #region Methods
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+        #endregion
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Banking.Models.Domain;
+using System;
 
 namespace Banking
 {
@@ -6,7 +7,15 @@ namespace Banking
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount myAccount = new BankAccount("123-123123-12");
+            Console.WriteLine(myAccount.Balance.ToString());
+            Console.WriteLine($"AccountNumber: {myAccount.AccountNumber}");
+            Console.WriteLine($"Balance: {myAccount.Balance}");
+            myAccount.Deposit(200M);
+            Console.WriteLine($"Balance after deposit of 200 euros: {myAccount.Balance}");
+            myAccount.Withdraw(100M);
+            Console.WriteLine($"Balance after withdrawal of 100 euros: {myAccount.Balance}");
+            Console.WriteLine(Console.ReadKey());
         }
     }
 }
