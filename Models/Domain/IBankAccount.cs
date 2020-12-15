e1bb24fdc0475d75f9b0768f59543636d54d1ddb
Victor Koopman <victor.koopman@student.hogent.be>
2020-12-15 16:47:13 +0100

    Add interface IBankAccount

---
+++ Models/Domain/IBankAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Banking.Models.Domain
+{
+    public interface IBankAccount
+    {
+        string AccountNumber { get; }
+        decimal Balance { get; }
+        int NumberOfTransactions { get; }
+
+        void Deposit(decimal amount, string notes = null);
+        IEnumerable<Transaction> GetTransactions(DateTime? from, DateTime? till);
+        void Withdraw(decimal amount, string notes = null);
+    }
+}
